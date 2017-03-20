ac544ff4470699ac95d370a72cc91a2b7f854802
bsc218 <bsc218@lehigh.edu>
2017-03-20 15:44:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ object.annual_property_taxes = 1;
 object.annual_insurance_costs = 1;
 object.interest_rate = 1;
 object.number_of_years = 1;
+
+function monthlyPayment () {
+	var m_int = interest_rate/12;
+    for(var i =0; i < number_of_years*12; i++) {
+        [
+  	    var monthly_amort= p*m_int;
+  	    var mPrincipal = mpmt-monthly_amort;
+  	    p=p-mPrincipal;
+  }
+}
